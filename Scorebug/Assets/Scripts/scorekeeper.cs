c3162b69cdabe22994a1c4db6e2fcdbac3a4f4f7
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-03-04 03:17:40 -0800

    Working Scorebug over UDP Sockets

--- Scorebug/Assets/Scripts/scorekeeper.cs
+++ Scorebug/Assets/Scripts/scorekeeper.cs
@@ -6,23 +6,94 @@ using System.Text;
 using System.Threading;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class scorekeeper : MonoBehaviour
 {
     public Dictionary<string, string> score;
     public Thread scoreKeeperServer;
 
+    // UI Elements
+    public TextMeshProUGUI visitorScore;
+    public TextMeshProUGUI homeScore;
+    public TextMeshProUGUI inning;
+    public RectTransform inningIndicator;
+    public TextMeshProUGUI balls;
+    public TextMeshProUGUI strikes;
+    public TextMeshProUGUI outsLabel;
+    public TextMeshProUGUI outs;
+    public RawImage firstBaseUI;
+    public RawImage secondBaseUI;
+    public RawImage thirdBaseUI;
+    public Color emptyBase;
+    public Color occupiedBase;
+
     // Start is called before the first frame update
     void Start()
     {
         scoreKeeperServer = new Thread(new ThreadStart(ScoreServer));
-        scoreKeeperServer.Start();
+        scoreKeeperServer.Start();        
     }
 
     // Update is called once per frame
     void Update()
     {
+        try
+        {
+            UpdateTextUI(visitorScore, "visitor");
+            UpdateTextUI(homeScore, "home");
+            UpdateTextUI(inning, "inning");
+            UpdateTextUI(balls, "balls");
+            UpdateTextUI(strikes, "strikes");
+            UpdateTextUI(outs, "outs");
+            
+            UpdateBaseUI(firstBaseUI, "base1");
+            UpdateBaseUI(secondBaseUI, "base2");
+            UpdateBaseUI(thirdBaseUI, "base3");
 
+            UpdateIndicator();
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Couldn't Update UI");
+        }
+    }
+
+
+    void UpdateTextUI(TextMeshProUGUI target, string field)
+    {
+        target.text = score[field];
+    }
+
+    void UpdateBaseUI(RawImage img, string field)
+    {
+        bool state = bool.Parse(score[field]);
+        if (state)
+        {
+            img.color = occupiedBase;
+        }
+        else
+        {
+            img.color = emptyBase;
+        }
+    }
+
+    void UpdateIndicator()
+    {
+        bool top = bool.Parse(score["top"]);
+        bool bottom = bool.Parse(score["bottom"]);
+
+        if (top && !bottom)
+        {
+            inningIndicator.eulerAngles = new Vector3(0.0f, 0.0f,00.0f);
+            
+        }
+
+        if (!top && bottom)
+        {
+            inningIndicator.eulerAngles = new Vector3(0.0f, 0.0f,180.0f);
+        }
     }
 
     void ScoreServer()
@@ -32,11 +103,12 @@ public class scorekeeper : MonoBehaviour
         IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
 
         string previous_payload = "";
+        string payload_string = "NONE";
 
         while (true)
         {
             var payload = server.Receive(ref remoteEP);
-            string payload_string = Encoding.UTF8.GetString(payload);
+            payload_string = Encoding.UTF8.GetString(payload);
             if (payload_string != previous_payload)
             {
                 score = JsonConvert.DeserializeObject<Dictionary<string, string>>(payload_string);
--- Scorekeeper/scorekeeper.py
+++ Scorekeeper/scorekeeper.py
@@ -1,4 +1,12 @@
+'''ScoreSnake Authoritative Scorekeeping Classes and Network Infrastructure'''
+import socket
+import json
+import time
+import threading
+
 class BaseballScorekeeper:
+    '''Scorekeeper for Baseball'''
+
     def __init__(self):
         # Scores
         self.visitor = 0
@@ -19,11 +27,39 @@ class BaseballScorekeeper:
         self.base2 = False
         self.base3 = False
 
+        # Networking
+        self.server = NetworkHandler('127.0.0.1', 42016)
+        self.server.send(self.package())
+
+    def package(self):
+        '''Package scorekeeper variables into a JSON string.'''
+        package = {
+            'visitor': self.visitor,
+            'home': self.home,
+            'inning': self.inning,
+            'top': self.top,
+            'bottom': self.bottom,
+            'balls': self.balls,
+            'strikes': self.strikes,
+            'outs': self.outs,
+            'base1': self.base1,
+            'base2': self.base2,
+            'base3': self.base3
+        }
+
+        return json.dumps(package)
+
+    def update(self):
+        '''Update connected server with new score data.'''
+        self.server.send(self.package())
+
     def strike(self):
         self.strikes += 1
         if self.strikes == 3: # Strikeout
             self.reset_count()
             self.out()
+
+        self.update()
         return self.strikes
 
     def ball(self):
@@ -40,6 +76,8 @@ class BaseballScorekeeper:
                     self.base2 = True
             else:
                 self.base1 = True
+
+        self.update()
         return self.balls
 
     def out(self):
@@ -47,32 +85,42 @@ class BaseballScorekeeper:
         if self.outs == 3: # End of At-Bat
             self.advance_game()
             self.outs = 0
+
+        self.update()
         return self.outs
 
     def minus_strike(self):
         self.strikes -= 1
         if self.strikes < 0:
             self.strikes = 0
+
+        self.update()
         return self.strikes
 
     def minus_ball(self):
         self.balls -= 1
         if self.balls < 0:
             self.balls = 0
+
+        self.update()
         return self.balls
 
     def minus_out(self):
         self.outs -= 1
         if self.outs < 0:
             self.outs = 0
+
+        self.update()
         return self.outs
 
     def reset_strikes(self):
         self.strikes = 0
+        self.update()
         return self.strikes
 
     def reset_balls(self):
         self.balls = 0
+        self.update()
         return self.balls
 
     def reset_count(self):
@@ -80,6 +128,7 @@ class BaseballScorekeeper:
 
     def reset_outs(self):
         self.outs = 0
+        self.update()
         return self.outs
 
     def base1_toggle(self):
@@ -87,6 +136,8 @@ class BaseballScorekeeper:
             self.base1 = False
         else:
             self.base1 = True
+
+        self.update()
         return self.base1
 
     def base2_toggle(self):
@@ -94,6 +145,8 @@ class BaseballScorekeeper:
             self.base2 = False
         else:
             self.base2 = True
+
+        self.update()
         return self.base2
 
     def base3_toggle(self):
@@ -101,6 +154,8 @@ class BaseballScorekeeper:
             self.base3 = False
         else:
             self.base3 = True
+
+        self.update()
         return self.base3
 
     def reset_bases(self):
@@ -108,12 +163,16 @@ class BaseballScorekeeper:
         self.base2 = False
         self.base3 = False
 
+        self.update()
+
     def score(self):
         if self.top:
             self.visitor += 1
+            self.update()
             return self.visitor
         elif self.bottom:
             self.home += 1
+            self.update()
             return self.home
 
     def minus_score(self):
@@ -121,33 +180,44 @@ class BaseballScorekeeper:
             self.visitor -= 1
             if self.visitor < 0:
                 self.visitor = 0
+            self.update()
             return self.visitor
         elif self.bottom:
             self.home -= 1
             if self.home < 0:
                 self.home = 0
+            self.update()
             return self.home
 
     def bottom_inning(self):
         self.bottom = True
         self.top = False
+
+        self.update()
         return (self.inning, self.top, self.bottom)
 
     def top_inning(self):
         self.top = True
         self.bottom = False
+
+        self.update()
         return (self.inning, self.top, self.bottom)
 
     def new_inning(self):
         self.inning += 1
         if self.bottom:
+            self.update()
             return self.top_inning()
+
+        self.update()
         return (self.inning, self.top, self.bottom)
 
     def old_inning(self):
         self.inning -= 1
         if self.inning < 1:
             self.inning = 1
+
+        self.update()
         return (self.inning, self.top, self.bottom)
 
     def advance_game(self):
@@ -157,4 +227,28 @@ class BaseballScorekeeper:
             self.new_inning()
         self.reset_count()
         self.reset_bases()
-    
+
+class NetworkHandler:
+    '''UDP Server and Network Handler'''
+
+    def __init__(self, server: str, port: int, ping=2.0):
+        self.target_ip = server
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        self.last_package = None
+
+        self.ping = ping
+        self.heartbeat = threading.Thread(target=self.keep_alive)
+        self.heartbeat.start()
+
+    def send(self, package: str):
+        '''Send score data over socket.'''
+        self.last_package = bytes(package, 'utf-8')
+        self.socket.sendto(self.last_package, (self.target_ip, self.port))
+
+    def keep_alive(self):
+        '''Periodically updates server with last package to account for packet loss.'''
+        while True:
+            time.sleep(self.ping)
+            self.socket.sendto(self.last_package, (self.target_ip, self.port))
