d851677bc1b754f5de15fd0f75d2e94d0a8b917d
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-03-03 23:27:22 -0800

    Scorekeeper UDP Server Framework

---
+++ Scorebug/Assets/Scripts/scorekeeper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class scorekeeper : MonoBehaviour
+{
+    public Dictionary<string, string> score;
+    public Thread scoreKeeperServer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreKeeperServer = new Thread(new ThreadStart(ScoreServer));
+        scoreKeeperServer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ScoreServer()
+    {
+        UdpClient server = new UdpClient(42016);
+
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+        string previous_payload = "";
+
+        while (true)
+        {
+            var payload = server.Receive(ref remoteEP);
+            string payload_string = Encoding.UTF8.GetString(payload);
+            if (payload_string != previous_payload)
+            {
+                score = JsonConvert.DeserializeObject<Dictionary<string, string>>(payload_string);
+                previous_payload = payload_string;
+            }
+        }
+    }
+}
