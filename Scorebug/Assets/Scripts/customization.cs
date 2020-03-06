b0ef1dc8318cf8c732cdc83104ba780cfd83ae2a
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-03-06 00:18:20 -0800

    Added Fullscreen/Windowed Toggle @ F2

--- Scorebug/Assets/Scripts/customization.cs
+++ Scorebug/Assets/Scripts/customization.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class customization : MonoBehaviour
 {
     public GameObject configCanvas;
+    private bool fullscreen = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        Screen.SetResolution(960, 540, FullScreenMode.Windowed);
     }
 
     // Update is called once per frame
@@ -18,6 +19,20 @@ public class customization : MonoBehaviour
         {
             configCanvas.SetActive(!configCanvas.activeSelf);
             Cursor.visible = configCanvas.activeSelf;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            if (fullscreen)
+            {
+                Screen.SetResolution(960, 540, FullScreenMode.Windowed);
+                fullscreen = false;
+            }
+            else
+            {
+                Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow);
+                fullscreen = true;
+            }
         }
     }
 }
