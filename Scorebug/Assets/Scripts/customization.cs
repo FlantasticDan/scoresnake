f1f97ad5a38a2ec3e0c2464c3ef61dc7ff6f570b
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-03-07 17:49:22 -0800

    Added Luma/Chroma Toggle + Scorebug Positioning

--- Scorebug/Assets/Scripts/customization.cs
+++ Scorebug/Assets/Scripts/customization.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class customization : MonoBehaviour
 {
     public GameObject configCanvas;
+    public RectTransform scoreCanvas;
+    public Camera mainCamera;
+    public Color chromaColor;
+    public Color lumaColor;
+    public RectTransform option5;
+    public RectTransform option4;
+    public RectTransform option3;
+    public RectTransform option2;
+    public RectTransform option1;
     private bool fullscreen = false;
+    private bool chroma = true;
+
     // Start is called before the first frame update
     void Start()
     {
         Screen.SetResolution(960, 540, FullScreenMode.Windowed);
+        mainCamera.backgroundColor = chromaColor;
     }
 
     // Update is called once per frame
@@ -33,6 +45,47 @@ public class customization : MonoBehaviour
                 Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow);
                 fullscreen = true;
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            if (chroma)
+            {
+                mainCamera.backgroundColor = lumaColor;
+                chroma = false;
+            }
+            else
+            {
+                mainCamera.backgroundColor = chromaColor;
+                chroma = true;
+            }
+        }
+
+        // Scorebug Positioning
+        if (Input.GetKeyDown(KeyCode.F8))
+        {
+            scoreCanvas.transform.position = option5.transform.position;
+            scoreCanvas.transform.localScale = option5.transform.localScale;
+        }
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            scoreCanvas.transform.position = option4.transform.position;
+            scoreCanvas.transform.localScale = option4.transform.localScale;
+        }
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            scoreCanvas.transform.position = option3.transform.position;
+            scoreCanvas.transform.localScale = option3.transform.localScale;
+        }
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            scoreCanvas.transform.position = option2.transform.position;
+            scoreCanvas.transform.localScale = option2.transform.localScale;
+        }
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            scoreCanvas.transform.position = option1.transform.position;
+            scoreCanvas.transform.localScale = option1.transform.localScale;
         }
     }
 }
